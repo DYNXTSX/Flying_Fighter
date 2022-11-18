12fb05925d4549e01baceae93fc4c929117b87ae
Maybachalex <maybachalex63@gmail.com>
2022-11-18 15:54:14 +0100

    Commentaires Ennemi.java

--- flyingcode/app/src/main/java/com/example/flyingfighter/Ennemi.java
+++ flyingcode/app/src/main/java/com/example/flyingfighter/Ennemi.java
@@ -8,6 +8,22 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 
+/**
+ * Classe [Ennemi]
+ * @description cette classe représente les ennemis dans le jeu
+ *
+ *
+ * (int) speed > vitesse des ennemis
+ * (int) x > position ennemi x
+ * (int) y > position ennemi y
+ * (int) width > largeur ennemi
+ * (int) height > taille ennemi
+ * (int) Ennemicounter > compteur d'ennemis
+ * (boolean) wasShot > booléen qui nous permet de savoir si l'ennemi a été shooté ou non
+ * (Bitmap) ufo1,ufo2,ufo3,ufo4 > Format d'image, (il y en a 4 pour la petite animation)
+ *
+ */
+
 public class Ennemi {
 
 
@@ -37,7 +53,7 @@ public class Ennemi {
         ufo3 = Bitmap.createScaledBitmap(ufo3, width, height, false);
         ufo4 = Bitmap.createScaledBitmap(ufo4, width, height, false);
 
-        y = -height;
+        y = -height; //Pour que l'ennemi soit placé en bas de l'écran au début du jeu
     }
 
     Bitmap getEnnemi () {
@@ -57,12 +73,12 @@ public class Ennemi {
             return ufo3;
         }
 
-        Ennemicounter = 1;
+        Ennemicounter = 1; //recommencer l'animation une fois qu'elle est finie comme on reprend à 1
 
         return ufo4;
     }
 
     Rect getCollisionShape () {
-        return new Rect(x, y, x + width, y + height);
+        return new Rect(x, y, x + width, y + height); //cette fonction retourne un objet de Rect (cela créé un rectangle autour de l'ennemi pour gérer les collisions)
     }
 }
