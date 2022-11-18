d86f085ad70e3927726e4141c4835d235cb8d864
Maybachalex <maybachalex63@gmail.com>
2022-11-18 16:57:20 +0100

    commmentaires Laser.java

--- flyingcode/app/src/main/java/com/example/flyingfighter/Laser.java
+++ flyingcode/app/src/main/java/com/example/flyingfighter/Laser.java
@@ -8,13 +8,17 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 
+/**
+ * Classe [Laser]
+ * @description cette classe représente les lasers que l'avion allié tire lorsqu'on clique sur l'écran.
+ */
 public class Laser {
 
     int x,y, width,height;
     Bitmap laser;
 
     Laser(Resources res) {
-        laser = BitmapFactory.decodeResource(res, R.drawable.laser);
+        laser = BitmapFactory.decodeResource(res, R.drawable.laser); //On créé le bitmap
 
         width = laser.getWidth();
         height = laser.getHeight();
@@ -25,11 +29,11 @@ public class Laser {
         width = (int) (width * screenRatioX );
         height = (int) (height * screenRatioY );
 
-        laser = Bitmap.createScaledBitmap(laser, width, height, false);
+        laser = Bitmap.createScaledBitmap(laser, width, height, false); //remise à l'échelle du bitmap existant
 
     }
 
     Rect getCollisionShape () {
         return new Rect(x, y, x + width, y + height);
-    }
+    } //Gestion des collisions avec Rect
 }
